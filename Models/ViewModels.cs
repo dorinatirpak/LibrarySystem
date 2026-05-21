9e7f8d22dbb1b1dcdf37998067ec0c1d0fa66491
dorinatirpak <dorinatirpak@gmail.com>
2026-05-21 20:46:57 +0200

    Add validation to member/loan view models and new EditMemberViewModel
    Added data annotations for validation in CreateLoanViewModel and CreateMemberViewModel. Introduced EditMemberViewModel with validation attributes. Added necessary using directive for data annotations.

--- Models/ViewModels.cs
+++ Models/ViewModels.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibrarySystem.Models;
 
 public class LoginViewModel
@@ -41,6 +43,9 @@ public class MemberLoansViewModel
 public class CreateLoanViewModel
 {
     public List<int> BookIds { get; set; } = [];
+    
+    [Required(ErrorMessage = "A tag kiválasztása kötelező!")]
+    [Range(1, int.MaxValue, ErrorMessage = "A tag kiválasztása kötelező!")]
     public int MemberId { get; set; }
     public DateTime LoanDate { get; set; } = DateTime.Today;
     public string? ErrorMessage { get; set; }
@@ -50,10 +55,41 @@ public class CreateLoanViewModel
 
 public class CreateMemberViewModel
 {
+    [Required(ErrorMessage = "A név megadása kötelező!")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "A névnek legalább 3 és legfeljebb 100 karakternek kell lennie!")]
+    [RegularExpression(@"^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ\s\.]+$", ErrorMessage = "A név csak betűket, szóközt és pontot tartalmazhat!")]
     public string Name { get; set; } = "";
+
+    [Required(ErrorMessage = "A lakcím megadása kötelező!")]
+    [StringLength(200, MinimumLength = 10, ErrorMessage = "A lakcímnek legalább 10 karakternek kell lennie!")]
     public string Address { get; set; } = "";
+
+    [Required(ErrorMessage = "Az elérhetőség megadása kötelező!")]
+    [EmailAddress(ErrorMessage = "Érvénytelen e-mail formátum!")]
     public string Contact { get; set; } = "";
+
+    [Required(ErrorMessage = "A típus kiválasztása kötelező!")]
     public MemberType MemberType { get; set; } = MemberType.Student;
+}
+
+public class EditMemberViewModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "A név megadása kötelező!")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "A névnek legalább 3 és legfeljebb 100 karakternek kell lennie!")]
+    [RegularExpression(@"^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ\s\.]+$", ErrorMessage = "A név csak betűket, szóközt és pontot tartalmazhat!")]
+    public string Name { get; set; } = "";
+
+    [Required(ErrorMessage = "A lakcím megadása kötelező!")]
+    [StringLength(200, MinimumLength = 10, ErrorMessage = "A lakcímnek legalább 10 karakternek kell lennie!")]
+    public string Address { get; set; } = "";
+
+    [Required(ErrorMessage = "Az elérhetőség megadása kötelező!")]
+    [EmailAddress(ErrorMessage = "Érvénytelen e-mail formátum!")]
+    public string Contact { get; set; } = "";
+    
+    public string? MemberTypeDisplay { get; set; }
 }
 
 public class DashboardViewModel
